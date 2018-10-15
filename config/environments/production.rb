2ecea8aaa521bb53aa85086e3fd154fa65ae91e0
Tigran Apoyan <apoyan.tigran@gmail.com>
2018-10-15 13:05:53 +0400

    try to configure redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://top-contributor.herokuapp.com',
+                                                 'http://top-contributor.herokuapp.com']
+
+  config.action_cable.url = "wss://top-contributor.herokuapp.com/cable"
 end
