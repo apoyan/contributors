fcb8fbe0beb2d4b3e02185caf0609d0c5191fecb
Tigran Apoyan <apoyan.tigran@gmail.com>
2018-10-15 13:27:54 +0400

    try to configure gush

--- config/initializers/gush.rb
+++ config/initializers/gush.rb
@@ -1,5 +1,5 @@
 Gush.configure do |config|
   config.redis_url = ENV.fetch("REDISTOGO_URL", "redis://localhost:6379")
-  config.concurrency = 2
+  config.concurrency = 1
   config.ttl = 3600*24*7
 end
